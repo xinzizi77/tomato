e01bdc2384c7ee94b8e2a4fbd34e8a4084cfa91f
jthvv <1187720942@@qq.com>
2018-06-02 14:36:14 +0800

    aa

--- pages/unlockimg/unlockimg.js
+++ pages/unlockimg/unlockimg.js
@@ -4,19 +4,19 @@ Page({
       {
         name: "教父",
         comment: "最精彩的剧本，最真实的黑帮电影。",
-        imagepath: "images/tu2.jpg",
+        imagepath: "http://193.112.31.67/images/bg3.jpeg",
         isHighlyRecommended: true//强烈推荐的判断
       },
       {
         name: "泰坦尼克号",
         comment: "失去的才是永恒的",
-        imagepath: "images/tu3.jpg",
+        imagepath: "http://193.112.31.67/images/bg1.jpg",
 
       },
       {
         name: "这个杀手不太冷",
         comment: "小萝莉和怪蜀黍纯真灿烂的爱情故事",
-        imagepath: "images/tu4.jpg",
+        imagepath: "http://193.112.31.67/images/bg2.jpg",
 
       },
     ],
